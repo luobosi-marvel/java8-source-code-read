830766008b971b5d5d4b0a891c8bfb8c3d69ce25
luobosi <luobosi@2dfire.com>
2018-09-07 17:08:41 +0800

    JUC 源码阅读

--- src/java/util/concurrent/ThreadPoolExecutor.java
+++ src/java/util/concurrent/ThreadPoolExecutor.java
@@ -1277,21 +1277,43 @@ public class ThreadPoolExecutor extends AbstractExecutorService {
      * Creates a new {@code ThreadPoolExecutor} with the given initial
      * parameters.
      *
-     * @param corePoolSize the number of threads to keep in the pool, even
-     *        if they are idle, unless {@code allowCoreThreadTimeOut} is set
-     * @param maximumPoolSize the maximum number of threads to allow in the
-     *        pool
-     * @param keepAliveTime when the number of threads is greater than
-     *        the core, this is the maximum time that excess idle threads
-     *        will wait for new tasks before terminating.
-     * @param unit the time unit for the {@code keepAliveTime} argument
-     * @param workQueue the queue to use for holding tasks before they are
-     *        executed.  This queue will hold only the {@code Runnable}
-     *        tasks submitted by the {@code execute} method.
-     * @param threadFactory the factory to use when the executor
-     *        creates a new thread
-     * @param handler the handler to use when execution is blocked
-     *        because the thread bounds and queue capacities are reached
+     * @param corePoolSize 线程池中核心线程的数量。
+     *                     当提交一个任务时，线程池会新建一个线程来执行任务，直到当前线程数等于corePoolSize。
+     *                     如果调用了线程池的prestartAllCoreThreads()方法，线程池会提前创建并启动所有基本线程。
+     *
+     * @param maximumPoolSize 线程池中允许的最大线程数。
+     *                        线程池的阻塞队列满了之后，如果还有任务提交，如果当前的线程数小于maximumPoolSize，则会新建线程来执行任务。
+     *                        注意，如果使用的是无界队列，该参数也就没有什么效果了。
+     *
+     * @param keepAliveTime 线程空闲的时间。
+     *                      线程的创建和销毁是需要代价的。线程执行完任务后不会立即销毁，而是继续存活一段时间：keepAliveTime。
+     *                      默认情况下，该参数只有在线程数大于corePoolSize时才会生效。
+     *
+     * @param unit keepAliveTime的单位。TimeUnit
+     *
+     * @param workQueue 用来保存等待执行的任务的阻塞队列
+     *                  ArrayBlockingQueue：基于数组结构的有界阻塞队列，FIFO。
+     *                  LinkedBlockingQueue：基于链表结构的有界阻塞队列，FIFO
+     *                  SynchronousQueue：不存储元素的阻塞队列，每个插入操作都必须等待一个移出操作，反之亦然。
+     *                  PriorityBlockingQueue：具有优先界别的阻塞队列。
+     *
+     * @param threadFactory 用于设置创建线程的工厂。该对象可以通过Executors.defaultThreadFactory()
+     *
+     * @param handler RejectedExecutionHandler，线程池的拒绝策略。
+     *                所谓拒绝策略，是指将任务添加到线程池中时，线程池拒绝该任务所采取的相应策略。
+     *                当向线程池中提交任务时，如果此时线程池中的线程已经饱和了，而且阻塞队列也已经满了，
+     *                则线程池会选择一种拒绝策略来处理该任务。
+     *
+     *                线程池提供了四种拒绝策略：
+     *                  - AbortPolicy：直接抛出异常，默认策略；
+     *                  - CallerRunsPolicy：用调用者所在的线程来执行任务；
+     *                  - DiscardOldestPolicy：丢弃阻塞队列中靠最前的任务，并执行当前任务；
+     *                  - DiscardPolicy：直接丢弃任务
+     *
+     *                  - 实现自己的拒绝策略，例如记录日志等等，实现RejectedExecutionHandler接口即可。
+     *
+     *
+     *
      * @throws IllegalArgumentException if one of the following holds:<br>
      *         {@code corePoolSize < 0}<br>
      *         {@code keepAliveTime < 0}<br>
